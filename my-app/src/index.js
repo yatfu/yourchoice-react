350db1ca8cb1e0336ee2370c016e0448c591849e
yatfu <55002354+yatfu@users.noreply.github.com>
2019-10-17 11:20:47 -0700

    Add files via upload

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class YourChoice extends React.Component {
 
     render() {
         return (
-            <h1>Island Snow!</h1>
+            <Container textAlign="center">
+        <Header as='h1'>YourChoice!</Header>
+        </Container>
     );
     }
 }
 
-ReactDOM.render(< YourChoice/>, document.getElementById('root'));
+ReactDOM.render(<YourChoice/>, document.getElementById('root'));
