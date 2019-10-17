2e44ff89fc31fac5762743cf1dd66e9ad49e05ce
yatfu <keanuyl@hawaii.edu>
2019-10-17 10:54:33 -0700

    Task 2

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class YourChoice extends React.Component {
+
+    render() {
+        return (
+            <h1>Island Snow!</h1>
+    );
+    }
+}
+
+ReactDOM.render(< YourChoice/>, document.getElementById('root'));
