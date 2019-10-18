773f0ada0cbeb76e4c34f218db794d120633d3d2
yatfu <55002354+yatfu@users.noreply.github.com>
2019-10-18 00:34:21 -0700

    Manual Upload
    GitHub desktop isn't working because of an error 128

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Menu, Grid, Image, Header } from 'semantic-ui-react';
+
+
+class TopMenu extends React.Component {
+
+    render() {
+
+        return (
+
+            <Menu borderless className="topmenu">
+
+            <Grid container centered>
+
+        <Menu.Item>HOME PAGE</Menu.Item>
+
+        <Menu.Item>LOCATIONS</Menu.Item>
+
+        <Menu.Item>ABOUT US</Menu.Item>
+
+        <Menu.Item>RESERVATIONS</Menu.Item>
+
+        <Menu.Item>CONTACT US</Menu.Item>
+
+        </Grid>
+
+        </Menu>)
+
+    }
+
+}
+
+class Background extends React.Component {
+    render() {
+        return (
+        <Container fluid>
+            <Image src='/src/tanaka.jpg' fluid/>
+        </Container>
+    )
+    }
+}
+
+
+class FooterMenu extends React.Component {
+    render() {
+        return (
+            <Image src='/tanaka.jpg'/>
+    )
+    }
+}
 
 class YourChoice extends React.Component {
 
     render() {
         return (
-            <Container textAlign="center">
-        <Header as='h1'>YourChoice!</Header>
-        </Container>
+            <div>
+            <TopMenu/>
+            <Background/>
+            <FooterMenu/>
+            </div>
     );
     }
 }
